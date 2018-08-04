f243ade7d489912bf8f05b9ddd22ad87e7c45f3c
Michael Lee Farringer <18293468@sun.ac.za>
2018-08-04 15:21:49 +0200

    Michael: Did ALO OF CHANGES EVERYWEHERE

--- src/Chat/Server.java
+++ src/Chat/Server.java
@@ -11,15 +11,25 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.*;
 import java.lang.String;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+
 import Chat.Client;
 
+import javax.swing.*;
 
 
 public class Server {
 
     private static Map<String, SocketChannel> nickNames = new HashMap<>();
-    private static Queue<Message> messages = new LinkedList<Message>();
+    private static Map<String, Lock> nickLocks = new HashMap<>();
+    private static Queue<Message> messages = new ConcurrentLinkedQueue<Message>();
     private static int BUFFER_SIZE = 1024;
+    private static Selector selector = null;
+    private static int count = 0;
+    private static Lock l1 = new ReentrantLock();
 
 
     Server() throws Throwable {
@@ -45,7 +55,7 @@ public class Server {
 
         // Setup the server connection
         // Selector is used to communicate with multiple channels.
-        Selector selector = Selector.open();
+        selector = Selector.open();
 
         // Initialising the server socket channel.
         ServerSocketChannel ssc = ServerSocketChannel.open();
@@ -57,6 +67,8 @@ public class Server {
 
         // Register channel
         ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+        System.out.println(" - Server Running");
 
 
         while (true) {
@@ -70,75 +82,12 @@ public class Server {
             while (iterator.hasNext()){
                 SelectionKey key = iterator.next();
                 iterator.remove();
+                count++;
+                thread(key);
 
-                if(key.isAcceptable()){
-                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
-                    // Get client socket channel
-                    SocketChannel client = server.accept();
-                    client.configureBlocking(false);
-
-                    // Register for read and write operations.
-                    client.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-
-                    Message m1 = Message.ServerRecieve(client);
-                    System.out.println("Message Name: " + m1.getSender());
-
-                    if(isUniqueName(m1.getSender())) {
-                        String msg = "Successful";
-                        Message m = new Message(msg, m1.getSender(), "GLOBAL");
-                        m.ServerSend(client);
-                        m = new Message(m1.getSender(), "USER_ADD");
-                        GlobalSend(m);
-                        m = new Message(nickNames.keySet().toString(), "USER_ADD_LIST");
-                        m.ServerSend(client);
-                        nickNames.put(m1.getSender(), client);
-
-                    } else {
-                        String msg = "Unsuccessful";
-                        Message m = new Message(msg, m1.getSender(), "GLOBAL");
-                        m.ServerSend(client);
-                    }
-
-                    continue;
-                }
-
-                // If readable then server is ready to read.
-                if(key.isReadable()){
-                    SocketChannel client = (SocketChannel) key.channel();
-
-                    try{
-                        Message msg = Message.ServerRecieve(client);
-                        if(msg.getMessage() == null && msg.getMessageType().equals("USER_ADD_LIST")){
-                            Message m = new Message(nickNames.keySet().toString(), "USER_ADD_LIST");
-                            m.ServerSend(client);
-                        } else if (msg.getMessageType().equals("DISCONN")) {
-                            System.out.println(msg.getMessage()+ " disconnected");
-                            GlobalSend(msg);
-                            key.cancel();
-                            key.channel().close();
-                            nickNames.remove(msg.getMessage());
-                        } else {
-                            messages.add(msg);
-                        }
-
-
-                    } catch(Exception E) {
-                        System.out.println("ERROR Reading.");
-                    }
-                }
-                // If write able then server is ready to write.
-                // This will be changed to a method that writes the data to another client.
-                else if(key.isWritable()){
-                    if(!messages.isEmpty()){
-                        Message msg  = messages.remove();
-                        if(msg.getReceiver().equals("GLOBAL")){
-                            GlobalSend(msg);
-                        }
-                        else {
-                            msg.ServerSend(nickNames.get(msg.getReceiver()));
-                        }
-                    }
-                }
+                while(count != 0){
+                    System.out.flush();
+                };
             }
         }
     }
@@ -147,12 +96,112 @@ public class Server {
         Iterator iter = nickNames.entrySet().iterator();
         while(iter.hasNext()) {
             Map.Entry client = (Map.Entry) iter.next();
-
+            nickLocks.get(client.getKey()).lock();
             if (!client.getKey().equals(msg.getSender())) {
                 SocketChannel cli = (SocketChannel) client.getValue();
                 msg.ServerSend(cli);
             }
+            nickLocks.get(client.getKey()).unlock();
         }
     }
+
+    public static void thread(SelectionKey key){
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                if(key.isAcceptable()) Accept(key);
+                // If readable then server is ready to read.
+                else if(key.isReadable()) Read(key);
+                // If able to write then server is ready to write.
+                else if(key.isWritable()) Write();
+                l1.lock();
+                count--;
+                l1.unlock();
+                return;
+            }
+        });
+        t1.start();
+    }
+
+    public static void Accept(SelectionKey key) {
+        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+        // Get client socket channel
+        SocketChannel client= null;
+        try{
+            client = server.accept();
+            client.configureBlocking(false);
+            // Register for read and write operations.
+            client.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        } catch (Exception E) {
+            System.out.println("Server could not connect to client - " + E);
+        }
+
+        Message m1 = Message.ServerRecieve(client);
+        System.out.print(" - User: " + m1.getSender() + " --> Requesting connection to Server --> ");
+
+        if(isUniqueName(m1.getSender())) {
+            nickLocks.put(m1.getSender(), new ReentrantLock());
+            String msg = "Successful";
+            System.out.println(msg);
+            Message m = new Message(msg, m1.getSender(), "GLOBAL");
+            m.ServerSend(client);
+            m = new Message(m1.getSender(), "USER_ADD");
+            GlobalSend(m);
+            nickNames.put(m1.getSender(), client);
+            m = new Message(nickNames.keySet().toString(), "USER_ADD_LIST");
+            m.ServerSend(client);
+
+
+        } else {
+            String msg = "Unsuccessful";
+            System.out.println(msg);
+            Message m = new Message(msg, m1.getSender(), "GLOBAL");
+            m.ServerSend(client);
+        }
+    }
+
+    public static void Read(SelectionKey key){
+        SocketChannel client = (SocketChannel) key.channel();
+
+        try{
+            if(client.isConnected()){
+                Message msg = Message.ServerRecieve(client);
+                if(msg.getMessage() == null && msg.getMessageType().equals("USER_ADD_LIST")){
+                    Message m = new Message(nickNames.keySet().toString(), "USER_ADD_LIST");
+                    m.ServerSend(client);
+                }  else if (msg.getMessageType().equals("DISCONN")) {
+                    System.out.println(msg.getMessage()+ " disconnected");
+                    GlobalSend(msg);
+                    key.cancel();
+                    key.channel().close();
+                    nickNames.remove(msg.getMessage());
+                } else {
+                    messages.add(msg);
+                }
+            }
+            // TODO disconnect
+            /*if(!msg.getMessageType().equals("DISCONN")) {
+                System.out.println(msg.getMessage()+ " disconnected");
+                key.cancel();
+                key.channel().close();
+            }*/
+
+        } catch(Exception E) {
+            System.out.println("ERROR Reading.");
+        }
+    }
+
+    public static void Write() {
+        if(!messages.isEmpty()){
+            Message msg  = messages.remove();
+            if(msg.getReceiver().equals("GLOBAL")){
+                GlobalSend(msg);
+            }
+            else {
+                msg.ServerSend(nickNames.get(msg.getReceiver()));
+            }
+        }
+    }
+
+
 }
 
